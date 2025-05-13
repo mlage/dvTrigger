dc6bdd648ca4a3fdc84185369dd54707afd9b51e
Marcos Lage <mlage@ic.uff.br>
2025-05-13 13:02:41 -0300

    bug fix.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 document.getElementById('trigger').addEventListener('click', function () {
-    const result =  parsedUrl = new URL(window.location.href).searchParams.get("fileid");
+    const result = new URL(window.location.href).searchParams.get("fileid");
+
+    if (result === null) {
+        document.getElementById('content').innerHTML = 'No fileid found in the URL.';
+        return;
+    }
+
     document.getElementById('content').innerHTML = `${result}`;
 });
